3267c2ad6afeb2b9c53210f7d9b119e230c84fb8
geochronology <54387644+geochronology@users.noreply.github.com>
2022-09-23 14:45:01 -0500

    search

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,16 @@
+import { Fragment } from 'react';
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/EventList';
+import EventsSearch from '../../components/events/EventsSearch';
+
 function AllEventsPage() {
+  const events = getAllEvents();
+
   return (
-    <div>
-      <h1>all events</h1>
-    </div>
+    <Fragment>
+      <EventsSearch />
+      <EventList items={events} />
+    </Fragment>
   );
 }
 
