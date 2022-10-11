b2e311f05432ebd6baabc6c0bd4cd35fe93bb49e
geochronology <54387644+geochronology@users.noreply.github.com>
2022-10-11 15:21:58 -0500

    added datafetching with static props to main page

---
+++ helpers/api-util.js
@@ -0,0 +1,25 @@
+export async function getAllEvents() {
+  const response = await fetch(
+    'https://nextjs-course-cb02f-default-rtdb.firebaseio.com/events.json'
+  );
+  const data = await response.json();
+  const events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+
+  console.log(response);
+
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+
+  console.log('allEvents:', allEvents);
+  return allEvents.filter((event) => event.isFeatured);
+}
