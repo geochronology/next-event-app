21a038b2b4c00e1b5520b5e3c22e23488c3299f6
geochronology <54387644+geochronology@users.noreply.github.com>
2022-10-20 12:18:48 -0500

    added universal meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        {/* title will apply to all pages -unless- it's overwritten */}
+        <title>Next Events</title>
+        <meta name='description' content='' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Component {...pageProps} />
